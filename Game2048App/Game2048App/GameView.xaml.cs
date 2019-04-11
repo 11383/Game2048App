1d5049ed0c178350176b2ed5339155dfba4bc809
Sebastian <SebastianSedzik@gmail.com>
2019-04-11 21:54:45 +0200

    Add GameView

---
+++ Game2048App/Game2048App/GameView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game2048App
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GameView : ContentView
+	{
+        private int padding = 10;
+		public GameView ()
+		{
+			InitializeComponent ();
+        }
+
+        private void GvRoot_SizeChanged(object sender, EventArgs e)
+        {
+            var size = Math.Min(gvRoot.Width, gvRoot.Height);
+            Console.WriteLine(size);
+            gvContainer.WidthRequest = gvContainer.HeightRequest = size - (padding * 2);
+        }
+    }
+}
