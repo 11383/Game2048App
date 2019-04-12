c8bac775c6619f2ca0d543146c251155af28535b
Sebastian Sędzik <SebastianSedzik@gmail.com>
2019-04-12 16:29:00 +0200

    Add GameCell

---
+++ Game2048App/Game2048App/GameCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Game2048App
+{
+    public partial class GameCell : ContentView
+    {
+        public GameCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Game2048App/Game2048App/GameView.xaml.cs
+++ Game2048App/Game2048App/GameView.xaml.cs
@@ -16,12 +16,30 @@ namespace Game2048App
 		public GameView ()
 		{
 			InitializeComponent ();
+
+            //cell1
+            //var cell = new GameCell();
+            //AbsoluteLayout.SetLayoutBounds(cell, new Rectangle(0, 50, 1, 1));
+
+            //cell2
+            //var cell2 = new GameCell();
+            //AbsoluteLayout.SetLayoutBounds(cell2, new Rectangle(52, 50, 1, 1));
+
+            //gvContainer.Children.Add(cell);
+            //gvContainer.Children.Add(cell2);
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) => {
+                var cell = (GameCell)s;
+                cell.TranslateTo(0, 50, 1000, Easing.CubicInOut);
+            };
+
+            gcTest.GestureRecognizers.Add(tapGestureRecognizer);
         }
 
         private void GvRoot_SizeChanged(object sender, EventArgs e)
         {
             var size = Math.Min(gvRoot.Width, gvRoot.Height);
-            Console.WriteLine(size);
             gvContainer.WidthRequest = gvContainer.HeightRequest = size - (padding * 2);
         }
     }
